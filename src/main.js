f818762dbd7de8c65907e5affd2ab95d14574304
Tungzhen <dongzz@aliyun.com>
2020-01-06 10:20:12 +0800

    导入vant并封装注册

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 注册vant组件
+import './utils/register-vant'
+
 Vue.config.productionTip = false
 
 new Vue({
