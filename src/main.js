9937ebf33fb9b4cc5887605c7624f7e165d7cacc
Tungzhen <dongzz@aliyun.com>
2020-01-08 11:19:34 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 动态设置 REM 基准值 (html标签字体大小)
+import 'amfe-flexible'
+
 // 注册vant组件
 import './utils/register-vant'
+
+// 加载自定义(全局)样式
+// 注意: 该样式文件要放到第三方样式之后
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
