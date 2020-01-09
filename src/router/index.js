a914ef6fbd6a6171c0e54795c8de3cf23dde8b7f
Tungzhen <dongzz@aliyun.com>
2020-01-09 09:49:19 +0800

    样式搭建及登录请求接口封装

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    // @ 是 src 目录的别名,这是 VueCLI 项目中特殊提供的
+    component: () => import('@/views/login')
+  }
+]
 
 const router = new VueRouter({
   routes
--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 
 // 加载
-import { Button } from 'vant'
+import { Button,
+  Cell,
+  CellGroup,
+  NavBar,
+  Field } from 'vant'
 // 注册
 Vue.use(Button)
+  .use(Cell)
+  .use(CellGroup)
+  .use(NavBar) // 导航栏
+  .use(Field) // 输入框
