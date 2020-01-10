35bcb347df8079b46bd5af3543211177fcbf892f
Tungzhen <dongzz@aliyun.com>
2020-01-10 12:51:34 +0800

    登录请求及登录提示

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 
 // 加载
-import { Button,
+import {
+  Button,
   Cell,
   CellGroup,
   NavBar,
-  Field } from 'vant'
+  Field,
+  Toast } from 'vant'
 // 注册
 Vue.use(Button)
   .use(Cell)
   .use(CellGroup)
   .use(NavBar) // 导航栏
   .use(Field) // 输入框
+  .use(Toast) // 轻提示
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,6 +7,7 @@
     <!-- 登陆表单 -->
     <van-cell-group>
       <van-field
+        v-model="user.mobile"
         required
         clearable
         label="手机号"
@@ -14,6 +15,7 @@
       />
 
       <van-field
+        v-model="user.code"
         label="验证码"
         placeholder="请输入验证码"
         required
@@ -25,25 +27,55 @@
     </van-cell-group>
 
     <div class="btn-wrap">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
     <!-- /登陆表单 -->
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
+
 export default {
   name: 'LoginPage',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // 1.获取表单数据
+      const user = this.user
+      // 2.表单验证
+
+      // 表单验证通过,开始 loading 动画
+      this.$toast.loading({
+        duration: 0, // 持续时间，0表示持续展示不停止
+        forbidClick: true, // 是否禁止背景点击
+        message: '登录中...' // 提示消息
+      })
+      // 3.请求提交
+      try {
+        const res = await login(user)
+        console.log(res)
+        this.$toast.success('登录成功')
+      } catch (err) {
+        console.log('登陆失败', err)
+        this.$toast.fail('登录失败')
+      }
+      // 4.根据接口返回结果执行后续处理
+    }
+  }
 }
 </script>
 
