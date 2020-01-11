b74ea9b29c38aaf65be65ba5f7b2e87c75681934
Tungzhen <dongzz@aliyun.com>
2020-01-11 20:17:46 +0800

    Token处理-存储Token

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // null { token, refresh_token }
+    user: JSON.parse(window.localStorage.getItem('user'))
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+
+      // 为了放置刷新数据丢失,这里海选哦把数据放到本地存储持久化
+      // 注意:本地存储只能存字符串,不能存对象
+      window.localStorage.setItem('user', JSON.stringify(state.user))
+    }
   },
   actions: {
   },
